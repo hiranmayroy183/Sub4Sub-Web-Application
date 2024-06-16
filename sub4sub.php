4d0c905aade08b4ac5ce81e556a1d5300a2eba2e
hiranmayroy183 <hiranmayroy183@gmail.com>
2024-06-16 10:32:10 +0600

    design changes

--- sub4sub.php
+++ sub4sub.php
@@ -1,14 +1,15 @@
 <?php include 'functions/header.php' ?>
 
 <div class="container mt-3">
-  <h2>Bordered Table</h2>
-  <p>The .table-bordered class adds borders on all sides of the table and the cells:</p>            
+  <h2>YouTube Channels to Subscribe</h2>
+  <p class="alert alert-secondary">This is the list of all available YouTube channels that you can subscribe to, and in return, you will be subscribed to them. It's a simple calculation. Learn how to do everything from here. <a href="faq.php">Learn me</a></p>            
   <table class="table table-bordered">
     <thead>
       <tr>
         <th>Username</th>
         <th>Channel Name</th>
         <th>Channel URL</th>
+        <th>Verify</th>
       </tr>
     </thead>
     <tbody>
