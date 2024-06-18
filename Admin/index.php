b9cb871fa28fb1f2d0d52a13bf3a415b073eb635
hiranmayroy183 <hiranmayroy183@gmail.com>
2024-06-18 22:09:25 +0600

    done!

--- Admin/index.php
+++ Admin/index.php
@@ -1,4 +1,14 @@
 <?php
-header("Location: adminLogin.php");
-die();
-?>
+// adminAbout.php
+include '../functions/header.php';
+include '../functions/db.php';
+include '../functions/auth.php';
+
+if (!isAdminLoggedIn()) {
+    header('Location: adminLogin.php');
+    exit;
+}
+else{
+    header('Location: adminDashboard.php');
+    
+}
